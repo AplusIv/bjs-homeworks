43da560873c8d56047f4d275e3373c199e4e7e4c
Andrei Ivanov <50slash50@gmail.com>
2020-06-23 07:41:27 +0300

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,29 @@
  */
 
 function averageMark(a, g, p) {
-	
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let taskResult = (a + g + p) / 3;
     // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+
+	return taskResult;// ваш ответ;
 }
 
 function sayHello(userName) {
-	
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+
+	return message;// ваш ответ;
 }
 
 function calculateFormula() {
-    
+ let x = 2;
+ let y = 22;
+ let z = 0;
+ let result = x * y + 5 * z + x - 1;
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+
+    return result// ваш ответ;
 }
