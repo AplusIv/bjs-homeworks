eb77024411c604286e846870ff789a7cac4de295
Andrei Ivanov <50slash50@gmail.com>
2020-07-03 07:32:35 +0300

    first commit data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,34 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+  let parsedPercent = parseInt(percent);
+  let parsedContribution = parseInt(contribution);
+  let parsedAmount = parseInt(amount);
+
+  if (Number.isNaN(parsedPercent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+  } else if (Number.isNaN(parsedContribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+  } else if (Number.isNaN(parsedAmount)) {
+    return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
+
+    //Нужно ли было проверять дату? Просто в поле с датой нельзя ввести не числа. Если да, то не совсем понимаю как. ПарсИнт, боюсь, поломает введённые значения, и не получится посчитать количество месяцев потомя
+
+  } else {
+  let credit = parsedAmount - parsedContribution;
+  let monthQuantity = (date.getFullYear() - new Date().getFullYear()) * 12 + date.getMonth() - new Date().getMonth();
+  let amountPerMonth = credit * ((percent / 100 / 12) + (percent / 100 / 12) / (Math.pow(1 + percent / 100 / 12, monthQuantity) - 1));
+  let totalAmount = (amountPerMonth * monthQuantity).toFixed(2);
+  console.log(totalAmount);
+  return Number(totalAmount);
+  }
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  if (Boolean(name)) {
+    return `Привет, мир! Меня зовут ${name}`;
+  } else {
+    return `Привет, мир! Меня зовут Аноним`;
+  }
 }
